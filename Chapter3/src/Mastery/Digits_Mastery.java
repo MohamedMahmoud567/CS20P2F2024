b9f114e83ba945c5c47a19ad01d0b85df9cccf18
38258003 <38258003@L815WD220021.edu.cbe.ab.ca>
2024-09-24 09:32:24 -0600

    9:32am

--- Chapter3/src/Mastery/Digits_Mastery.java
+++ Chapter3/src/Mastery/Digits_Mastery.java
@@ -23,59 +23,32 @@ public class Digits_Mastery {
 		  //Create new scanner
         Scanner input = new Scanner(System.in);
 
-        //Values of each digit
-        int hundreds = 0;
-        int tens = 0;
-        int ones = 0;
-
-        //Prompt user to input 3 digit number           
+        int hundreds;
+        int tens;
+        int ones;
+         
         System.out.print("Please enter a 3 digit number: ");
         int number = input.nextInt();
 
-if (number <= 999 && number > 99)   // Checking condition for three digit number
-{
-
-        //Displays hundreds place digit
         hundreds = number / 100;
-        System.out.printf("Hundreds place digit: " + hundreds);
+        System.out.print(" Hundreds place digit: " + hundreds );
 
-        //Displays tens digit
-        tens = (number - (hundreds*100)) / 10;  // compare with your code
-        System.out.printf("\nTens place digit: " + tens);
+        tens = (number - (hundreds*100)) / 10;  
+        System.out.print(" Tens place digit: " + tens );
+    
+        ones = (number - (tens*10) - (hundreds*100));   
+        System.out.print(" Ones place digit: " + ones );   
 
-
-        //Display ones digit
-        ones = (number - (tens*10) - (hundreds*100));   // compare with your code
-        System.out.printf("\nOnes place digit: " + ones);   
-}
-                         //Error if number is less or more then three digits
-else                                                                    
-{      
-if (number > 999)
-        System.out.println("Sorry number is more then 3 digits.");
-        if (number < 100)
-        System.out.println("Sorry number less then 3 digits.");
 }
 
 
 
 }
-}
-
-
-
 
 /* Screen Dump
 
 
-Test Case 1:  Please enter a 3 digit number: 23
-Sorry number less then 3 digits.
-
-
-
-
-
-Test Case 2: Please enter a 3 digit number: 547
+Test case: Please enter a 3 digit number: 547
 Hundreds place digit: 5
 Tens place digit: 4
 Ones place digit: 7
@@ -84,6 +57,3 @@ Ones place digit: 7
 
  
  */
-
-
-
---
+++ Chapter3/src/Mastery/ElectionMastery.java
@@ -0,0 +1,12 @@
+package Mastery;
+
+public class ElectionMastery {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 System.out.print(": ");
+		 
+	}
+
+}
