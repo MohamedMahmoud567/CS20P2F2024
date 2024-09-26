ce2352b0539ea1c39dae050cc760e9c6abbd3c45
38258003 <38258003@L815WD220021.edu.cbe.ab.ca>
2024-09-26 09:46:08 -0600

    9:46am

---
+++ Chapter4/src/src/Mastery/GuessingGame1.java
@@ -0,0 +1,66 @@
+/*
+
+Program: Guessing game           Date: September 26, 2024
+
+Purpose: A guessing word game.
+
+
+School: CHHS
+Course: Computer Science 20
+ 
+*/
+
+package src.Mastery;
+
+import java.util.Scanner;
+
+public class GuessingGame1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+ String guess;
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+		String secretWord = "Apple";
+		
+		System.out.println("Please guess the word:  ");
+		
+	
+				guess = input.next();
+				
+				
+				if(guess.equalsIgnoreCase(secretWord))
+				{
+					System.out.println("You got the word correct");
+				}
+				else
+				{
+					System.out.println("You've guessed it incorrectly");
+				}
+				
+	}
+}
+
+/*
+Screen dump test case 1
+
+Please guess the word:  
+cheese
+You've guessed it incorrectly
+
+
+
+
+Test case 2
+
+
+Please guess the word:  
+apple
+You got the word correct
+
+
+ 
+*/
