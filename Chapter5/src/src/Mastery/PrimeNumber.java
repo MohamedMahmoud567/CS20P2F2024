5a5046f4b16f611b76297ec4199f2f43a9be54a9
38258003 <38258003@L815WD220021.edu.cbe.ab.ca>
2024-10-16 10:34:11 -0600

    10:34am

---
+++ Chapter5/src/src/Mastery/PrimeNumber.java
@@ -0,0 +1,67 @@
+/*
+
+Program: Prime Number         Date: October 15, 2024
+
+Purpose: Create an application that promots the user for a number and display whether its prime or not
+
+Author: Mohamed Mahmoud
+School: CHHS
+Course: Computer Programming 20
+ 
+
+*/
+
+package src.Mastery;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+
+		int X;
+		
+		boolean isPrime=true;
+		
+		Scanner scan= new Scanner(System.in);
+		
+		System.out.println("Enter any number:");
+		
+		int num=scan.nextInt();
+	        scan.close();
+		
+	     for(int i=2;i<=num/2;i++)
+		{
+	     
+	    	 
+	    	 X=num%i;
+		   if(X==0)
+		   {
+		      isPrime=false;
+		     
+		      
+		    
+		   }
+		}
+		
+		
+	     if(isPrime)
+		   System.out.println(num + " is a Prime Number");
+		else
+		   System.out.println(num + " is not a Prime Number");
+	   }
+	
+    }
+
+
+
+	
+	
+
+	
+
+
+	
+	
